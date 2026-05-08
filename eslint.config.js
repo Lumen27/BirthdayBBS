2c25800a5df408c75b1ac1495a91fc60f78b4819
mitin <mitin.mark@yandex.ru>
2026-05-08 22:50:59 +0200

    prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,35 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import react from "eslint-plugin-react";
+import hooks from "eslint-plugin-react-hooks";
+import refresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import prettier from "eslint-config-prettier";
 
-export default defineConfig([
-  globalIgnores(['dist']),
+export default [
+  { ignores: ["dist"] },
+
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      globals: globals.browser,
+    files: ["**/*.{ts,tsx}"],
+    plugins: {
+      react,
+      "react-hooks": hooks,
+      "react-refresh": refresh,
+    },
+    rules: {
+      ...react.configs.recommended.rules,
+      ...hooks.configs.recommended.rules,
+      "react/react-in-jsx-scope": "off", // Vite + React
+      "react-refresh/only-export-components": "warn",
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
     },
   },
-])
+
+  prettier,
+];
